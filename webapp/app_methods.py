c050a164242851ff26db574fb7e086b66a34c5a0
Thomas Mahoney <thomas.mahoney@ons.gsi.gov.uk>
2018-05-08 09:30:35 +0100

    Fixed error in run creation by adding run type value to CSV output.

--- webapp/app_methods.py
+++ webapp/app_methods.py
@@ -10,8 +10,8 @@ def create_run(unique_id,run_name, run_description, start_date, end_date):
 
     :return: NA
     """
-
-    new_entry = str(unique_id) + "," + run_name + "," + run_description + "," + start_date + "," + end_date + "," + "0" + "\n"
+    
+    new_entry = str(unique_id) + "," + run_name + "," + run_description + "," + start_date + "," + end_date + "," + "0" +  "," + "6" + "\n"
 
     f = open('../webapp/resources/run_list.csv', 'a')
     f.write(new_entry)
