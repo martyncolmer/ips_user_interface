6cfd403db73591e4bb2eef11307ad3fdd63375b6
James Burr <burrj@statistics.gov.uk>
2018-05-25 10:51:09 +0100

    Add new_run_2 view function
    Adding a single view function in this commit to separate out/demonstrate
    the addition of a single variant view function to an already functional
    Blueprint. Following commits will include more than one view function.

--- webapp/new_run.py
+++ webapp/new_run.py
@@ -1,5 +1,6 @@
 from flask import request, render_template, Blueprint, session, redirect, url_for
-from .forms import CreateRunForm
+from .forms import CreateRunForm, DateSelectionForm
+from . import app_methods
 import uuid
 
 bp = Blueprint('new_run', __name__, url_prefix='/new_run', static_folder='static')
@@ -16,7 +17,65 @@ def new_run_1():
             session['id'] = str(unique_id)
             session['run_name'] = request.form['run_name']
             session['run_description'] = request.form['run_description']
-            return redirect(url_for('new_run_2'), code=302)
+            return redirect(url_for('new_run.new_run_2'), code=302)
 
     return render_template('/projects/legacy/john/social/new_run_1.html',
                            form=form)
+
+
+@bp.route('/new_run_2', methods=['GET', 'POST'])
+def new_run_2():
+    form = DateSelectionForm()
+
+    print(request.values)
+
+    # if request is a post
+    if request.method == 'POST':
+        session['s_day'] = request.form['s_day']
+        session['s_month'] = request.form['s_month']
+        session['s_year'] = request.form['s_year']
+        session['e_day'] = request.form['e_day']
+        session['e_month'] = request.form['e_month']
+        session['e_year'] = request.form['e_year']
+
+        if 'last_name' in session:
+            last_name = session['last_name']
+
+        if form.validate():
+            if request.form['submit'] == 'create_run':
+                start_date = request.form['s_day'] + request.form['s_month'] + request.form['s_year']
+                end_date = request.form['e_day'] + request.form['e_month'] + request.form['e_year']
+
+                session['start_date'] = start_date
+                session['end_date'] = end_date
+
+                app_methods.create_run(session['id'], session['run_name'], session['run_description'],
+                                       session['start_date'], session['end_date'])
+
+                return redirect(url_for('new_run.new_run_3'), code=302)
+        else:
+            pass
+            #flash_errors(form)
+
+    last_entry = {}
+
+    if 's_day' in session:
+        last_entry['s_day'] = session['s_day']
+        last_entry['s_month'] = session['s_month']
+        last_entry['s_year'] = session['s_year']
+        last_entry['e_day'] = session['e_day']
+        last_entry['e_month'] = session['e_month']
+        last_entry['e_year'] = session['e_year']
+        print("Found session s_day")
+    else:
+        last_entry['s_day'] = ""
+        last_entry['s_month'] = ""
+        last_entry['s_year'] = ""
+        last_entry['e_day'] = ""
+        last_entry['e_month'] = ""
+        last_entry['e_year'] = ""
+        print("FOUND NOTHING")
+
+    return render_template('/projects/legacy/john/social/new_run_2.html',
+                           form=form,
+                           last_entry=last_entry)
